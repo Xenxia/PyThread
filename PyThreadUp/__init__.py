3cd2f0bfe02147a89991377b2b1fb56a8a63194e
xenxia <xen.devops@gmail.com>
2022-12-04 15:18:08 +0100

    rewrite ThreadUp Add ThreadManager In ThreadUp add kill Thread

--- PyThreadUp/__init__.py
+++ PyThreadUp/__init__.py
@@ -1,28 +1,111 @@
 from threading import Thread
+import threading, sys
+from typing import Any
 
-class ThreadUP(Thread):
 
-    returnValue: bool
-
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None, returnValue=False):
-        Thread.__init__(self, group, target, name, args, kwargs)
+class ThreadUP(threading.Thread):
+    def __init__(self, returnValue=False, *args, **keywords):
+        threading.Thread.__init__(self, *args, **keywords)
+        self.killed = False
         self.returnValue = returnValue
-        self._return = None
 
     def __join(self, *args):
         if self.is_alive():
             Thread.join(self, *args)
         if self.returnValue:
-            return self._return
-        # return
+            try:
+                return self._return
+            except:
+                return None
 
+    def __run(self):
+        sys.settrace(self.globaltrace)
+        self.__run_backup()
+        self.run = self.__run_backup
+    
     def run(self):
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
 
+    def start(self):
+        self.__run_backup = self.run
+        self.run = self.__run
+        threading.Thread.start(self)
+
     def join(self, *args):
         return self.__join(*args)
+    
+    def globaltrace(self, frame, event, arg):
+        if event == 'call':
+            return self.localtrace
+        else:
+            return None
+    
+    def localtrace(self, frame, event, arg):
+        if self.killed:
+            if event == 'line':
+                raise SystemExit()
+            return self.localtrace
 
-    def startTask(self):
-        self.start()
-        return self
+    def kill(self):
+        self.killed = True
+
+
+class ThreadManager():
+
+    threads: dict[str, ThreadUP]
+
+    def __init__(self) -> None:
+        self.threads = {}
+
+    def thread(self, name: str, target: Any | None, args: Any = (), returnValue: bool = False):
+
+        self.threads[name] = ThreadUP(returnValue=returnValue, name=name, target=target, args=args)
+
+    def start(self, name: str):
+
+        t = self.get_thread(name)
+        try:
+            t.start()
+        except RuntimeError as e:
+            self.threads[name] = ThreadUP(name=t._name, target=t._target, args=t._args, returnValue=t.returnValue)
+            self.threads[name].start()
+
+    def join(self, name: str) -> Any | None:
+        
+        return self.get_thread(name).join()
+
+    def kill(self, name: str):
+        
+        t = self.get_thread(name)
+        t.kill()
+
+        self.threads[name] = ThreadUP(name=t._name, target=t._target, args=t._args, returnValue=t.returnValue)
+
+    def kill_all(self):
+        
+        for name, t in self.threads.items():
+            t.kill()
+            self.threads[name] = ThreadUP(name=t._name, target=t._target, args=t._args, returnValue=t.returnValue)
+
+    def is_alive(self, name: str) -> bool:
+        
+        return self.get_thread(name).is_alive()
+
+    def get_thread(self, name: str) -> ThreadUP:
+
+        try:
+            return self.threads[name]
+        except:
+            raise(f"Thread name {name} not existe")
+
+    def get_threads(self) -> dict[str, ThreadUP]:
+
+        return self.threads
+
+    def remove(self, name: str):
+
+        try:
+            del self.threads[name]
+        except:
+            raise(f"Thread name {name} not existe")
--- tUp.py
+++ tUp.py
@@ -1,86 +1,23 @@
-from threading import Thread, Event
-import threading
-import time, sys
-import random
-from xmlrpc.client import Boolean
+import time
+from PyThreadUp import ThreadManager
+
+def func(t):
+    for i in range(90000):
+        print(i)
+
+    return t
+ 
+tm = ThreadManager()
+
+tm.thread(name="test", target=func, returnValue=True, args=("test thread",))
+tm.start("test")
+time.sleep(2)
+tm.kill("test")
+print("k")
+time.sleep(2)
+tm.start("test")
+print(tm.join("test"))
 
 
-class ThreadUP(Thread):
-
-    returnValue: bool
-
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None, returnValue=False):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self.returnValue = returnValue
-        self._return = None
-
-    def __join(self, *args):
-        if self.is_alive():
-            Thread.join(self, *args)
-        if self.returnValue:
-            return self._return
-        # return
-
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
-
-    def join(self, *args):
-        return self.__join(*args)
-
-    def startJoin(self, *args):
-        self.start()
-        return self.__join(*args)
-
-class Test():
-
-    def __init__(self) -> None:
-        self.val = random.randint(0, 500)
-        time.sleep(1)
-
-    def returnVal(self):
-        return self.val
-
-print(f"start at {time.strftime('%X')}")
-
-def test(time_n: int):
-    for i in range(0, time_n):
-        time.sleep(1)
-    return time_n
-
-task1 = ThreadUP(target=Test, returnValue=True)
-task2 = ThreadUP(target=test, args=(2,), returnValue=True)
-
-try:
-    task1.start()
-    task2.start()
-except:
-    print(sys.exc_info()[0])
-
-
-print(task1.join().returnVal())
-print(task2.join())
-
-
-print(f"finished at {time.strftime('%X')}")
-
-
-
-class Toto:
-
-    def __init__(self) -> None:
-        self.fruit = dict({
-            "toto": 0,
-            "titi": 1,
-            "tutu": 2,
-            "tata": "toto",
-            "tyty": 4,
-        })
-
-toto = Toto()
-
-
-tutu = sorted([*{f"test-{k}": v for k, v in toto.fruit.items() if v is not None}])
-
-
-print(( tutu))
+if not tm.is_alive("test"):
+    print('thread killed')
